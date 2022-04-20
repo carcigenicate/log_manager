b1039253642a741fe01e3f4ee5c67b048313049b
carcigenicate <brendonw5@gmail.com>
2022-04-20 09:31:58 -0600

    Divided the log into three models: LoggedUsers which themselves only hold an ID; LogEntry which hold the time of the entry, the type of the entry, and any associated property data; and LoggedSession which records what sessions the user what associated with.

--- log_manager/models.py
+++ log_manager/models.py
@@ -1,2 +1,24 @@
 from django.db import models
 
+
+class LoggedUser(models.Model):
+    id = models.CharField(max_length=10)
+
+
+class LogEntry(models.Model):
+    time = models.DateTimeField()
+
+    # If there's a closed-set of types, this would benefit from including a models.TextChoices "enum"
+    # The sample is insufficient to determine that though.
+    type = models.CharField(max_length=50)
+
+    # There does not seem to be a consistent "shape" to the property data, so storing it as raw JSON seems appropriate.
+    properties = models.JSONField()
+
+    user = models.ForeignKey(LoggedUser, on_delete=models.SET_NULL)
+
+
+class LoggedSession(models.Model):
+    id = models.CharField(max_length=10)
+
+    user = models.ForeignKey(LoggedUser, on_delete=models.SET_NULL)
