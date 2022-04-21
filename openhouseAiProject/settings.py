d66bdaa175b037a8eea2f5ac138f6bb160b4ef2f
carcigenicate <brendonw5@gmail.com>
2022-04-20 21:10:11 -0600

    Attempting to use the heroku's auto-configuration library to see if it resolves the database issues.

--- openhouseAiProject/settings.py
+++ openhouseAiProject/settings.py
@@ -12,6 +12,8 @@ https://docs.djangoproject.com/en/4.0/ref/settings/
 import sys
 from pathlib import Path
 
+import django_heroku
+
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -130,3 +132,5 @@ REST_FRAMEWORK = {
     'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.PageNumberPagination',
     'PAGE_SIZE': 20
 }
+
+django_heroku.settings(locals())
